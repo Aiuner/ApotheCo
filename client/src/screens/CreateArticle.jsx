ff62893e2d5532382fc78f084b9e3cc7df69ea9d
Aiuner <demonspawndaemnyxea@gmail.com>
2020-09-17 16:11:17 -0400

    Not sure why I can't type in the CreateArticle form's fields.

--- client/src/screens/CreateArticle.jsx
+++ client/src/screens/CreateArticle.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory as history } from 'react-router-dom';
+
+import { postArticle } from '../services/articles.js';
 
 export default function CreateArticle() {
+  const [articles, setArticles] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "",
+    content: ""
+  })
+  const { title, content } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const submitArticle = async (formData) => {
+    const newArticle = await postArticle(formData);
+    setArticles(prevState => [...prevState, newArticle]);
+    history.push('/articles/index')
+  }
+
   return (
-    <h1>This is the article creation page.</h1>
+    <form onSubmit={submitArticle}>
+      <h1>New Article</h1>
+      <label>
+        Title:
+        <input 
+          type="text"
+          value={title}
+          onChange={handleChange} />
+      </label>
+      <label>
+        Content:
+        <input 
+          type="text"
+          value={content}
+          onChange={handleChange} />
+      </label>
+      <button>Submit</button>
+    </form>
   );
 }

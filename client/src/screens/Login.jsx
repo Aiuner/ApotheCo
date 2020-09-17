bafd1e9d314feb96a64892a64b8b33fdab884297
Aiuner <demonspawndaemnyxea@gmail.com>
2020-09-17 09:16:38 -0400

    Login and Register screens have functionality.

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Login() {
+export default function Login(props) {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  })
+  const { username, password } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   return (
-    <h1>This is the alogin page.</h1>
-  );
+    <form onSubmit={(e)=> {
+      e.preventDefault();
+      props.loginSubmit(formData);
+    }}>
+      <h3>Login</h3>
+      <label>Username:
+        <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>Password:
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <Link to='/register'>Register</Link>
+      <button>Submit</button>
+    </form>
+  )
 }
